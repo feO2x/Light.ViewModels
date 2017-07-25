b8629a292ccffeb66f7f75f22335a41fd6fd050d
Kenny Pflug <kenny.pflug@live.de>
2017-07-25 07:24:50 +0200

    added DelegateCommand and corresponding tests

---
+++ Code/Light.ViewModels.Tests/DelegateCommandTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows.Input;
+using FluentAssertions;
+using Xunit;
+
+namespace Light.ViewModels.Tests
+{
+    public sealed class DelegateCommandTests
+    {
+        private static readonly Action DoNothing = () => { };
+
+        [Fact]
+        public void DelegateCommandImplementsICommand()
+        {
+            typeof(DelegateCommand).Should().Implement<ICommand>();
+        }
+
+        [Fact]
+        public void CanExecuteReturnsTrueWhenNoFuncWasPassedIn()
+        {
+            var testTarget = new DelegateCommand(DoNothing);
+
+            var result = testTarget.CanExecute();
+
+            result.Should().BeTrue();
+            testTarget.CanExecuteFunc.Should().BeNull();
+        }
+
+        [Fact]
+        public void ExecuteMustCallTheAction()
+        {
+            var callCount = 0;
+            var testTarget = new DelegateCommand(() => callCount++);
+
+            testTarget.Execute();
+
+            callCount.Should().Be(1);
+        }
+
+        [Fact]
+        public void CanExecuteMustCallTheFunc()
+        {
+            var callCount = 0;
+            var testTarget = new DelegateCommand(DoNothing,
+                                                 () =>
+                                                 {
+                                                     callCount++;
+                                                     return false;
+                                                 });
+
+            var result = testTarget.CanExecute();
+
+            result.Should().BeFalse();
+            callCount.Should().Be(1);
+        }
+
+        [Fact]
+        public void ActionNotNull()
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            Action act = () => new DelegateCommand(null);
+
+            act.ShouldThrow<ArgumentNullException>()
+               .And.ParamName.Should().Be("execute");
+        }
+
+        [Fact]
+        public void RaiseCanExecuteChangedMustRaiseEvent()
+        {
+            var testTarget = new DelegateCommand(DoNothing);
+            var callCount = 0;
+            testTarget.CanExecuteChanged += (sender, args) => callCount++;
+
+            testTarget.RaiseCanExecuteChanged();
+
+            callCount.Should().Be(1);
+        }
+    }
+}
---
+++ Code/Light.ViewModels/DelegateCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Input;
+using Light.GuardClauses;
+
+namespace Light.ViewModels
+{
+    public sealed class DelegateCommand : ICommand
+    {
+        public readonly Func<bool> CanExecuteFunc;
+        public readonly Action ExecuteAction;
+
+        public DelegateCommand(Action execute, Func<bool> canExecute = null)
+        {
+            ExecuteAction = execute.MustNotBeNull(nameof(execute));
+            CanExecuteFunc = canExecute;
+        }
+
+        public void Execute()
+        {
+            ExecuteAction();
+        }
+
+        public bool CanExecute()
+        {
+            return CanExecuteFunc?.Invoke() ?? true;
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return CanExecute();
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            ExecuteAction();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
