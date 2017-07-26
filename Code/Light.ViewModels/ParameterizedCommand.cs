78506a61b2597003ee775d8d3df6dadf767ae8d9
Kenny Pflug <kenny.pflug@live.de>
2017-07-26 07:32:10 +0200

    added ParameterizedCommand

--- Code/Light.ViewModels/ParameterizedCommand.cs
+++ Code/Light.ViewModels/ParameterizedCommand.cs
@@ -5,63 +5,53 @@ using Light.GuardClauses;
 namespace Light.ViewModels
 {
     /// <summary>
-    ///     Represents an <see cref="ICommand" /> that calls parameterless delegates.
+    ///     Represents an <see cref="ICommand" /> that calls parameterized delegates.
     /// </summary>
-    public class DelegateCommand : ICommand
+    public class ParameterizedCommand : ICommand
     {
         /// <summary>
         ///     Gets the delegate that is executed when <see cref="CanExecute" /> is called. This value might be null.
         /// </summary>
-        public readonly Func<bool> CanExecuteFunc;
+        public readonly Func<object, bool> CanExecuteFunc;
 
         /// <summary>
         ///     Gets the delegate that is executed when <see cref="Execute" /> is called.
         /// </summary>
-        public readonly Action ExecuteAction;
+        public readonly Action<object> ExecuteAction;
 
         /// <summary>
-        ///     Initializes a new instance of <see cref="DelegateCommand" />.
+        ///     Initializes a new instance of <see cref="ParameterizedCommand" />
         /// </summary>
         /// <param name="execute">The delegate that will be executed when <see cref="Execute" /> is called.</param>
-        /// <param name="canExecute">The delegate that will be executed when <see cref="CanExecute" /> is called (optional).</param>
+        /// <param name="canExecute">the delegate that will be executed when <see cref="CanExecute" /> is called (optional).</param>
         /// <exception cref="ArgumentNullException">Thrown when <paramref name="execute" /> is null.</exception>
-        public DelegateCommand(Action execute, Func<bool> canExecute = null)
+        public ParameterizedCommand(Action<object> execute, Func<object, bool> canExecute = null)
         {
             ExecuteAction = execute.MustNotBeNull(nameof(execute));
             CanExecuteFunc = canExecute;
         }
 
-        bool ICommand.CanExecute(object parameter)
+        /// <summary>
+        ///     Checks if the command can be executed.
+        /// </summary>
+        public virtual bool CanExecute(object parameter)
         {
-            return CanExecute();
+            return CanExecuteFunc?.Invoke(parameter) ?? true;
         }
-
-        void ICommand.Execute(object parameter)
-        {
-            Execute();
-        }
-
-        /// <inheritdoc />
-        public event EventHandler CanExecuteChanged;
 
         /// <summary>
         ///     Executes the command.
         /// </summary>
         /// <exception cref="InvalidOperationException">Thrown when this method is called, but <see cref="CanExecute" /> returns false.</exception>
-        public virtual void Execute()
+        public virtual void Execute(object parameter)
         {
-            if (CanExecute() == false)
+            if (CanExecute(parameter) == false)
                 throw new InvalidOperationException("Execute must not be called when CanExecute returns false.");
-            ExecuteAction();
+            ExecuteAction(parameter);
         }
 
-        /// <summary>
-        ///     Checks if the command can be executed.
-        /// </summary>
-        public virtual bool CanExecute()
-        {
-            return CanExecuteFunc?.Invoke() ?? true;
-        }
+        /// <inheritdoc />
+        public event EventHandler CanExecuteChanged;
 
         /// <summary>
         ///     Raises the <see cref="CanExecuteChanged" /> event.
