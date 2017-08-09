d0782b770eed13ef072f94260be63b8e55213169
Kenny Pflug <kenny.pflug@live.de>
2017-08-09 21:57:11 +0200

    added ParameterizedCommand<T>

---
+++ Code/Light.ViewModels.Tests/ParameterizedCommandOfTTests.cs
@@ -0,0 +1,134 @@
+using System;
+using System.Reflection;
+using System.Windows.Input;
+using FluentAssertions;
+using Xunit;
+
+namespace Light.ViewModels.Tests
+{
+    public sealed class ParameterizedCommandOfTTests
+    {
+        [Fact]
+        public void ImplementICommand()
+        {
+            typeof(ParameterizedCommand<>).Should().Implement<ICommand>();
+        }
+
+        [Fact]
+        public void NotSealed()
+        {
+            typeof(ParameterizedCommand<>).GetTypeInfo().IsSealed.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ExecuteCallsTheDelegate()
+        {
+            var receivedParameter = default(string);
+            var testTarget = new ParameterizedCommand<string>(parameter => receivedParameter = parameter);
+
+            const string givenParameter = "Foo";
+            testTarget.Execute(givenParameter);
+
+            receivedParameter.Should().BeSameAs(givenParameter);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void CanExecuteCallsTheDelegate(bool canExecuteOutcome)
+        {
+            var receivedParameter = default(int);
+            var testTarget = new ParameterizedCommand<int>(_ => { },
+                                                           parameter =>
+                                                           {
+                                                               receivedParameter = parameter;
+                                                               return canExecuteOutcome;
+                                                           });
+
+            const int givenParameter = 42;
+            var result = testTarget.CanExecute(givenParameter);
+
+            receivedParameter.Should().Be(givenParameter);
+            result.Should().Be(canExecuteOutcome);
+        }
+
+        [Fact]
+        public void RaiseCanExecuteChangedRaisesTheEvent()
+        {
+            var testTarget = new ParameterizedCommand<object>(_ => { });
+            var callCount = 0;
+            testTarget.CanExecuteChanged += (s, a) => callCount++;
+
+            testTarget.RaiseCanExecuteChanged();
+
+            callCount.Should().Be(1);
+        }
+
+        [Fact]
+        public void ActionNotNull()
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            Action act = () => new ParameterizedCommand<object>(null);
+
+            act.ShouldThrow<ArgumentNullException>()
+               .And.ParamName.Should().Be("execute");
+        }
+
+        [Fact]
+        public void ExecuteMustThrowWhenCanExecuteReturnsFalse()
+        {
+            var testTarget = new ParameterizedCommand<string>(_ => { }, _ => false);
+
+            Action act = () => testTarget.Execute(null);
+
+            act.ShouldThrow<InvalidOperationException>()
+               .And.Message.Should().Be("Execute must not be called when CanExecute returns false.");
+        }
+
+        [Fact]
+        public void Inheritance()
+        {
+            var testTarget = new InheritanceMock(_ => { });
+
+            testTarget.Execute(null);
+            testTarget.CanExecute(null);
+            testTarget.RaiseCanExecuteChanged();
+
+            testTarget.AllOverriddenMethodsShouldHaveBeenCalled();
+        }
+
+        public sealed class InheritanceMock : ParameterizedCommand<string>
+        {
+            private int _canExecuteCallCount;
+            private int _executeCallCount;
+            private int _raiseCanExecuteChangedCallCount;
+
+            public InheritanceMock(Action<string> execute, Func<string, bool> canExecute = null) : base(execute, canExecute) { }
+
+            public override void Execute(string parameter)
+            {
+                base.Execute(parameter);
+                _executeCallCount++;
+            }
+
+            public override bool CanExecute(string parameter)
+            {
+                _canExecuteCallCount++;
+                return base.CanExecute(parameter);
+            }
+
+            public override void RaiseCanExecuteChanged()
+            {
+                base.RaiseCanExecuteChanged();
+                _raiseCanExecuteChangedCallCount++;
+            }
+
+            public void AllOverriddenMethodsShouldHaveBeenCalled()
+            {
+                _executeCallCount.Should().Be(1);
+                _canExecuteCallCount.Should().Be(2, "because Execute calls CanExecute, too, before invoking the delegate");
+                _raiseCanExecuteChangedCallCount.Should().Be(1);
+            }
+        }
+    }
+}
--- Code/Light.ViewModels/ParameterizedCommand.cs
+++ Code/Light.ViewModels/ParameterizedCommand.cs
@@ -61,4 +61,73 @@ namespace Light.ViewModels
             CanExecuteChanged?.Invoke(this, EventArgs.Empty);
         }
     }
+
+    /// <summary>
+    ///     Represents an <see cref="ICommand" /> that calls delegates with a strongly typed parameter.
+    /// </summary>
+    /// <typeparam name="T">The type of the parameter that will be passed to the delegates.</typeparam>
+    public class ParameterizedCommand<T> : ICommand
+    {
+        /// <summary>
+        ///     Gets the delegate that is executed when <see cref="CanExecute" /> is called. This value might be null.
+        /// </summary>
+        public readonly Func<T, bool> CanExecuteFunc;
+
+        /// <summary>
+        ///     Gets the delegate that is executed when <see cref="Execute" /> is called.
+        /// </summary>
+        public readonly Action<T> ExecuteAction;
+
+        /// <summary>
+        ///     Initializes a new instance of <see cref="ParameterizedCommand{T}" />
+        /// </summary>
+        /// <param name="execute">The delegate that will be executed when <see cref="Execute" /> is called.</param>
+        /// <param name="canExecute">the delegate that will be executed when <see cref="CanExecute" /> is called (optional).</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="execute" /> is null.</exception>
+        public ParameterizedCommand(Action<T> execute, Func<T, bool> canExecute = null)
+        {
+            ExecuteAction = execute.MustNotBeNull(nameof(execute));
+            CanExecuteFunc = canExecute;
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return CanExecute((T) parameter);
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            Execute((T) parameter);
+        }
+
+        /// <inheritdoc />
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        ///     Executes the command.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when this method is called, but <see cref="CanExecute" /> returns false.</exception>
+        public virtual void Execute(T parameter)
+        {
+            if (CanExecute(parameter) == false)
+                throw new InvalidOperationException("Execute must not be called when CanExecute returns false.");
+            ExecuteAction(parameter);
+        }
+
+        /// <summary>
+        ///     Checks if the command can be executed.
+        /// </summary>
+        public virtual bool CanExecute(T parameter)
+        {
+            return CanExecuteFunc?.Invoke(parameter) ?? true;
+        }
+
+        /// <summary>
+        ///     Raises the <see cref="CanExecuteChanged" /> event.
+        /// </summary>
+        public virtual void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
 }
