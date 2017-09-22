a94ffb42b25dda454c4af6da2848bb78b24c65bb
Kenny Pflug <kenny.pflug@live.de>
2017-09-22 10:03:01 +0200

    added NamedCommand

---
+++ Code/Light.ViewModels.Tests/NamedCommandTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Light.ViewModels.Tests
+{
+    public sealed class NamedCommandTests
+    {
+        private static readonly Action DoNothing = () => { };
+
+        [Fact]
+        public void DerivesFromDelegateCommand()
+        {
+            typeof(NamedCommand).Should().BeDerivedFrom<DelegateCommand>();
+        }
+
+        [Theory]
+        [InlineData("Foo")]
+        [InlineData("Bar")]
+        [InlineData("Baz")]
+        public void RetrieveName(string name)
+        {
+            var testTarget = new NamedCommand(name, DoNothing);
+
+            testTarget.Name.Should().BeSameAs(name);
+        }
+
+        [Fact]
+        public void NameNull()
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            Action act = () => new NamedCommand(null, DoNothing);
+
+            act.ShouldThrow<ArgumentNullException>()
+               .And.ParamName.Should().Be("name");
+        }
+    }
+}
---
+++ Code/Light.ViewModels/NamedCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Light.GuardClauses;
+
+namespace Light.ViewModels
+{
+    /// <summary>
+    ///     Represents a <see cref="DelegateCommand" /> that has a name.
+    /// </summary>
+    public class NamedCommand : DelegateCommand
+    {
+        /// <summary>
+        ///     Initializes a new instance of <see cref="NamedCommand" />.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="execute">The delegate that will be executed when <see cref="DelegateCommand.Execute" /> is called.</param>
+        /// <param name="canExecute">The delegate that will be executed when <see cref="DelegateCommand.CanExecute" /> is called (optional).</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="execute" /> is null.</exception>
+        public NamedCommand(string name, Action execute, Func<bool> canExecute = null) : base(execute, canExecute)
+        {
+            Name = name.MustNotBeNull(nameof(name));
+        }
+
+        /// <summary>
+        ///     Gets the name of the command.
+        /// </summary>
+        public string Name { get; }
+    }
+}
