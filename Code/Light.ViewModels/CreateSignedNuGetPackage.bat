065dc0fc8a0c516a3d0aad28cebd4328d471b897
Kenny Pflug <kenny.pflug@live.de>
2018-07-11 08:11:01 +0200

    updated version to 0.3.1

--- Code/Light.ViewModels/CreateSignedNuGetPackage.bat
+++ Code/Light.ViewModels/CreateSignedNuGetPackage.bat
@@ -1,1 +1,1 @@
-msbuild Light.ViewModels.csproj /t:rebuild /t:pack /p:Configuration=Release /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=bin\Light.ViewModels.snk
+msbuild Light.ViewModels.csproj /t:clean;build;pack /p:Configuration=Release /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=bin\Light.ViewModels.snk
